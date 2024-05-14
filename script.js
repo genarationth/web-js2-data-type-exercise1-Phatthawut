5c2de04569b66c2a1acb50ab41187f1d4e293a48
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-14 11:43:51 +0700

    exercise1 Create an information

---
+++ script.js
@@ -0,0 +1,5 @@
+let name_ex1 = 'John Slack';
+let location_ex1 = 'Bangkok';
+let hobby_ex1 = 'Reading all day';
+
+console.log(`Hi, my name is ${name_ex1}. I live in ${location_ex1} and enjoy ${hobby_ex1}.`);
